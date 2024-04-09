ffe2af74c2e048dec604acb0021e2afc239bf367
Elias Jackson <ejthedj0@gmail.com>
2024-04-09 14:49:32 -0700

    Add script

---
+++ README.md
@@ -0,0 +1,7 @@
+## Usage
+
+```
+npx tsx main.ts
+```
+
+For testing purposes only! Do not abuse this script by sending too many requests.
---
+++ main.ts
@@ -0,0 +1,32 @@
+import { readFileSync, writeFileSync } from 'fs';
+import { tabletojson } from 'tabletojson';
+
+const extractHref = (anchorElement: string) => {
+  const attributeSplit = anchorElement.split('"');
+  const hrefIndex = attributeSplit.findIndex((str) => str.includes('href'));
+  return attributeSplit[hrefIndex + 1];
+};
+
+const formatArtist = (artist) => {
+  const { Artist, Genre, Instagram, Spotify } = artist;
+  return {
+    name: Artist,
+    genre: Genre,
+    instagram: extractHref(Instagram),
+    spotify: extractHref(Spotify),
+  };
+};
+
+(async () => {
+  const response = await fetch('https://www.pdxpresents.com/portland-artists/');
+  const html = await response.text();
+  writeFileSync('portland-artists.html', html);
+
+  // uncomment to use cached data instead
+  // const html = readFileSync('portland-artists.html').toString();
+
+  const jsonTables = tabletojson.convert(html, { stripHtmlFromCells: false });
+  const artists = jsonTables[0].map(formatArtist);
+
+  writeFileSync('artists.json', JSON.stringify(artists, null, 2));
+})();
